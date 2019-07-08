e8db894dd4fa7b900a93d9485f880f0f1f3e81e3
Lucius Mathametica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-08 19:48:54 +0100

    tweaked map setView

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 
 $( document ).ready(function() {
 
-	var mymap = L.map('mapid').setView([51.275, 0.35], 13);
+	var mymap = L.map('mapid').setView([51.5, -0.13], 10);
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapbox_access_token, {
 	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
